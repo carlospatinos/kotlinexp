058274871a734e0b1c25376572c934bf4117eb99
carlospatinos <carlospatinos@gmail.com>
2017-11-27 16:52:55 +0000

    Adding some callable example

--- src/com/test/MyCallable.java
+++ src/com/test/MyCallable.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class MyCallable implements Callable<String> {
-
+    synchronized int a;
     @Override
     public String call() throws Exception {
         Thread.sleep(1000);
---
+++ src/sum/Sum.kt
@@ -0,0 +1,6 @@
+package sum
+
+fun sum(a: IntArray): Int {
+    // Write your solution here
+    return 0
+}
